f4b671733d71b1cc3da7e3cb673c1e38d5a3e297
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-14 16:36:35 +0900

    create UserSerializer

--- api_user/serializers.py
+++ api_user/serializers.py
@@ -3,3 +3,15 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from core.models import Profile, FriendRequest
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
