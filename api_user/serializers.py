198bbdddd7beb3c27c4c3b3f823401f13dee7e75
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-14 16:32:26 +0900

    import modules

---
+++ api_user/serializers.py
@@ -0,0 +1,5 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from core.models import Profile, FriendRequest
+
