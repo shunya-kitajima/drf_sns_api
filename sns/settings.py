d14c4c29ff7e4815ea21139a71687b47a392615c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-14 11:36:09 +0900

    add setting of AUTH_USER_MODEL

--- sns/settings.py
+++ sns/settings.py
@@ -111,6 +111,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+AUTH_USER_MODEL = 'core.User'
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
