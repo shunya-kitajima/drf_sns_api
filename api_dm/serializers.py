8fe57bf1e1f14a6b341d25899597ac69d783a284
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-15 19:09:34 +0900

    create FriendsFilter

--- api_dm/serializers.py
+++ api_dm/serializers.py
@@ -3,3 +3,22 @@ from rest_framework import serializers
 from core.models import User, Profile, Message, FriendRequest
 
 
+class FriendsFilter(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        request = self.context['request']
+        friends = FriendRequest.objects.filter(Q(askTo=request.user) & Q(approved=True))
+        list_friend = []
+        for friend in friends:
+            list_friend.append(friend.askFrom.id)
+        queryset = User.objects.filter(id__in=list_friend)
+        return queryset
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    receiver = FriendsFilter()
+
+    class Meta:
+        model = Message
+        fields = ('id', 'sender', 'receiver', 'message')
+        extra_kwargs = {'sender': {'read_only': True}}
+
