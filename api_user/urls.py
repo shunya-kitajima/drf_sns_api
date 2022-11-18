5159413b530fee98a23073e2c3845165260ed309
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 11:31:44 +0900

    set router and urlpatterns

--- api_user/urls.py
+++ api_user/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from api_user import views
 
 app_name = 'user'
 
 router = DefaultRouter()
+router.register('profile', views.ProfileViewSet)
+router.register('approval', views.FriendRequestViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('myprofile/', views.MyProfileListView.as_view(), name='myprofile'),
 ]
