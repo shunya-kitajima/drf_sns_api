9924bbe6ca22d0be6f1f4aad2ce745ce46a875b1
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 10:39:25 +0900

    import modules

--- api_user/views.py
+++ api_user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.db.models import Q
+from rest_framework import generics, authentication, permissions, viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+from api_user import serializers
+from core.models import Profile, FriendRequest
 
-# Create your views here.
+
+
