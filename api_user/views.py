6bcdb517ee01534d5eaa5f45fa4273a269e3875a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 10:51:59 +0900

    create FriendRequestViewSet

--- api_user/views.py
+++ api_user/views.py
@@ -11,3 +11,25 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = serializers.UserSerializer
 
 
+class FriendRequestViewSet(viewsets.ModelViewSet):
+    queryset = FriendRequest.object.all()
+    serializer_class = serializers.FriendRequestSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(Q(askTo=self.request.user) | Q(askFrom=self.request.user))
+
+    def perform_create(self, serializer):
+        try:
+            serializer.save(askFrom=self.request.user)
+        except:
+            raise ValidationError('User can have only unique request')
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'Delete is not allowed!'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'Patch is not allowed!'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
