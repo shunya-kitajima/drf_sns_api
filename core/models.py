136446aeed049534bf146727534b61c32f6c069b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 13:00:18 +0900

    modify and create user

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 def upload_path(instance, filename):
     ext = filename.split('.')[-1]
-    return '/'.join(['image'], str(instance.userPro.id) + str(instance.nickName) + str('.') + str(ext))
+    return '/'.join(['image', str(instance.userPro.id) + str(instance.nickName) + str('.') + str(ext)])
 
 
 class UserManager(BaseUserManager):
