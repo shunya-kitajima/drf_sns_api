f404f31f532d64c96b287145f0c197564c6887c0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 11:16:36 +0900

    create MessageViewSet

--- api_dm/views.py
+++ api_dm/views.py
@@ -1,8 +1,28 @@
 from django.db.models import Q
-from rest_framework import generics, authentication, permissions, viewsets, status
+from rest_framework import authentication, permissions, viewsets, status
 from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError
 from api_dm import serializers
 from core.models import Message
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = serializers.MessageSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(Q(askFrom=self.request.user))
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'Delete is not allowed!'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'Patch is not allowed!'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
