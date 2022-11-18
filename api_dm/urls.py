eb818be9378b6d5903a72410044d5d85cd1d5084
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 11:36:22 +0900

    set router

--- api_dm/urls.py
+++ api_dm/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from api_dm import views
 
 app_name = 'dm'
 
 router = DefaultRouter()
+router.register('message', views.MessageViewSet, basename='message')
+router.register('inbox', views.InboxListView, basename='inbox')
 
 urlpatterns = [
     path('', include(router.urls))
